4b1afa57e6209aa5f48da563233291b50c9fb84c
picard <picard@emse.fr>
2018-04-20 12:21:59 +0200

    new exhausitve anim

--- convexhull/test.py
+++ convexhull/test.py
@@ -1,17 +1,21 @@
 from random import seed
 
+from eddyfloyd import eddyfloyd
 from exhaustive import exhaustive
+from incremental import incremental
+from shamos import shamos
 from utils import create_points, scatter_plot, is_convex
 
 
 def main():
     seed(0)
-    pts = create_points(8)
+    pts = create_points(100)
+    # pts = [[0,25],[26,50],[25,25], [25,0], [50,25]]
     show = True  # to display a frame
     save = False  # to save into .png files in "figs" directory
     scatter_plot(pts, [[]], title="convex hull : initial set", show=show, save=save)
     print("Points:", pts)
-    hull = exhaustive(pts, show=show, save=save)
+    hull = eddyfloyd(pts, show=show, save=save)
     print("Hull:", hull)
     scatter_plot(pts, [hull], title="convex hull : final result", show=True, save=save)
 
