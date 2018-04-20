5745e27d7b695c59d33d5c2205f3a124598b64f5
picard <picard@emse.fr>
2018-04-20 14:50:14 +0200

    more doc

--- convexhull/test.py
+++ convexhull/test.py
@@ -1,21 +1,24 @@
 from random import seed
 
-from eddyfloyd import eddyfloyd
 from exhaustive import exhaustive
-from incremental import incremental
-from shamos import shamos
-from utils import create_points, scatter_plot, is_convex
-
+from utils import create_points, scatter_plot
 
 def main():
+    """
+    A sample main program to test our algorithms.
+
+    :return: None
+    """
+    # initialize the random generator seed to always use the same set of points
     seed(0)
-    pts = create_points(100)
-    # pts = [[0,25],[26,50],[25,25], [25,0], [50,25]]
+    # creates some points
+    pts = create_points(8)
     show = True  # to display a frame
     save = False  # to save into .png files in "figs" directory
     scatter_plot(pts, [[]], title="convex hull : initial set", show=show, save=save)
     print("Points:", pts)
-    hull = eddyfloyd(pts, show=show, save=save)
+    # compute the hull
+    hull = exhaustive(pts, show=show, save=save)
     print("Hull:", hull)
     scatter_plot(pts, [hull], title="convex hull : final result", show=True, save=save)
 
