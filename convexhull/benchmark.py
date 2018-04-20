bdc7445a188cad7a9084f2cf402fa0437c29d0b4
picard <picard@emse.fr>
2018-04-20 14:45:27 +0200

    more doc

--- convexhull/benchmark.py
+++ convexhull/benchmark.py
@@ -1,35 +1,45 @@
 from random import seed
 from time import time
 
-from graham import graham
-from jarvis import jarvis
-from shamos import shamos
 from exhaustive import exhaustive
 from utils import *
 
 
-# For each size in the 'sizes' list, compute the average
-# time to find the Convex Hull for a dataset of that size,
-# the range used for max and min for the create_points function
-# is always 10 times the highest value in the 'sizes' list.
-def benchmark(sizes=(10, 100, 1000, 10000, 100000), method=exhaustive):
+#
+def benchmark(sizes=(10, 100, 1000, 10000, 100000), runs=100, method=exhaustive):
+    """
+    For each size in the 'sizes' list, compute the average time over a given number of runs to find the convex hull
+    for a dataset of that size,
+    the range used for max and min for the create_points function is always 10 times the highest value in the 'sizes'
+    list.
+
+    :param sizes: list of problem sizes to consider (default is (10, 100, 1000, 10000, 100000))
+    :param method: the name of the algorithm to use (default is exhaustive)
+    :param runs: the number of repetition to perform for computing average (default is 100)
+    :return: nothing
+    """
     print(method)
     for s in sizes:
         tot = 0.0
-        for _ in range(3):
+        for _ in range(runs):
             points = create_points(s, 0, max(sizes) * 10)
             t0 = time.time()
             method(points, False)
             tot += (time.time() - t0)
-        print("size %d time: %0.5f" % (s, tot / 3.0))
+        print("size %d time: %0.5f" % (s, tot / float(runs)))
 
 
 def main():
+    """
+    A sample main program.
+
+    :return: nothing
+    """
     seed(0)
     algorithms = [exhaustive]  # [graham, jarvis, shamos]
 
     for algorithm in algorithms:
-        benchmark(sizes=[2, 4, 6, 8, 10], method=algorithm)
+        benchmark(sizes=(2, 4, 6, 8, 10), runs=10, method=algorithm)
 
 
 if __name__ == "__main__":
--- convexhull/exhaustive.py
+++ convexhull/exhaustive.py
@@ -1,24 +1,30 @@
-# Returns the vertices comprising the boundaries of
-# convex hull containing all points in the input set.
-# The input 'points' is a list of (x,y) coordinates.
-# If 'show_progress' is set to True, the progress in
-# constructing the hull will be plotted on each iteration.
-# Uses a very naive method: iterates over the whole set of convex polygons for size 3 to n
-
 from itertools import permutations
 
 from utils import is_convex, scatter_plot, point_in_polygon
 
 
 def exhaustive(points, show=True, save=False):
+    """
+    Returns the vertices comprising the boundaries of convex hull containing all points in the input set.
+    The input 'points' is a list of [x,y] coordinates.
+    Uses a very naive method: iterates over the whole set of convex polygons from size 3 to n
+
+    :param points: the points from which to find the convex hull
+    :param show: if True, the progress in constructing the hull will be plotted on each iteration in a window
+    :param save: if True, the progress in constructing the hull will be saved on each iteration in a .png file
+    :return: the convex hull
+    """
     i = 3
     while i <= len(points):
+        # iterates over the whole set of subset of points
         for subset in permutations(points, i):
+            # only consider convex subsets
             if is_convex(subset):
                 if show or save:
                     scatter_plot(points, [subset], title="exhaustive search", show=show, save=save)
                 one_out = False
                 j = 0
+                # iterates until a point is found outside the polygon
                 while not one_out and j < len(points):
                     point = points[j]
                     if point not in list(subset) and not point_in_polygon(point, list(subset)):
